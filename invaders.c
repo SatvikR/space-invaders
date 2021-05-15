0da0799829823599eca219bcd5a4cd70ea3dd0a6
Satvik Reddy <reddy.satvik@gmail.com>
2021-05-14 17:59:11 -0700

    created window

--- invaders.c
+++ invaders.c
@@ -1,7 +1,41 @@
-#include <stdio.h>
+#include <LCGE/lcge.h>
 
 int main(int argc, char const *argv[])
 {
-	printf("hello, world\n");
+	// Initialize LCGE with the path to the res directory of LCGE (specific to you)
+	if (lcge_init("lib/LCGE/res") == LCGE_INIT_ERR) {
+		lcge_exit();
+		return -1;
+	}
+
+	// Create a window
+	int success = lcge_create_context(500, 500, "LCGE Example",
+					  LCGE_NON_RESIZEABLE);
+
+	// Create clock with a max fps
+	LCGE_clock *clock = lcge_clock_create(60);
+
+	// Check if there was an error creating the window
+	if (success == LCGE_CONTEXT_ERR) {
+		lcge_exit();
+		return -1;
+	}
+
+	while (lcge_window_is_open()) {
+		// Do any udpating here
+
+		// Do any drawing here
+		lcge_window_clear();
+
+		// Get ready for next iteration
+		lcge_clock_tick(clock);
+		lcge_window_update();
+	}
+
+	// Clean up
+	lcge_clock_delete(clock);
+
+	lcge_exit();
+
 	return 0;
 }
