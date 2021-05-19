f838e3cdb36debff9d69c2aae6d2a2412a64945c
Satvik Reddy <reddy.satvik@gmail.com>
2021-05-18 22:28:32 -0700

    feat: added player sprite
    The player sprite will now be loaded on startup and can be moved side to
    side with the arrow keys

--- invaders.c
+++ invaders.c
@@ -1,39 +1,89 @@
+/*
+ * Copyright (c) 2021, Satvik Reddy
+ * Use of this source code is governed by a BSD-style license that can be found
+ * in the LICENSE file
+ */
+
 #include <LCGE/lcge.h>
+
+#define WIDTH  800
+#define HEIGHT 800
+
+#define CANNON_W (52.0f * 1.5f)
+#define CANNON_H (32.0f * 1.5f)
+
+#define PLAYER_SPEED 10.0f
+
+static LCGE_image *player_sprite;
+
+static void load_sprites()
+{
+	player_sprite = lcge_image_load("assets/cannon-small.png", (WIDTH - CANNON_W) / 2.0f, 700.0f ,CANNON_W, CANNON_H);
+}
+
+static void delete_sprites()
+{
+	lcge_image_delete(player_sprite);
+}
+
+/* Global state */
+static struct {
+	float x;
+} player_state;
 
 int main(int argc, char const *argv[])
 {
-	// Initialize LCGE with the path to the res directory of LCGE (specific to you)
+	/* Initialize LCGE with the path to the res directory of LCGE (specific to you) */
 	if (lcge_init("assets/") == LCGE_INIT_ERR) {
 		lcge_exit();
 		return -1;
 	}
 
-	// Create a window
-	int success = lcge_create_context(500, 500, "LCGE Example",
+	/* Create a window */
+	int success = lcge_create_context(WIDTH, HEIGHT, "Space Invaders",
 					  LCGE_NON_RESIZEABLE);
 
-	// Create clock with a max fps
-	LCGE_clock *clock = lcge_clock_create(60);
-
-	// Check if there was an error creating the window
+	/* Check if there was an error creating the window */
 	if (success == LCGE_CONTEXT_ERR) {
 		lcge_exit();
 		return -1;
 	}
 
-	while (lcge_window_is_open()) {
-		// Do any udpating here
+	player_state.x = (WIDTH - CANNON_W) / 2.0f;
 
-		// Do any drawing here
+	/* Create clock with a max fps */
+	LCGE_clock *clock = lcge_clock_create(60);
+
+	load_sprites();
+
+	while (lcge_window_is_open()) {
+		/* Udpating */
+		if (lcge_get_key(LCGE_KEY_LEFT) == LCGE_KEY_PRESSED) {
+			if (player_state.x >= PLAYER_SPEED) {
+				player_state.x -= PLAYER_SPEED;
+			}
+		}
+		if (lcge_get_key(LCGE_KEY_RIGHT) == LCGE_KEY_PRESSED) {
+			if ((player_state.x + CANNON_W) <= (WIDTH - PLAYER_SPEED)) {
+				player_state.x += PLAYER_SPEED;
+			}
+		}
+
+		lcge_image_set(player_sprite, player_state.x, 700.0f, CANNON_W, CANNON_H);
+		/* Drawing */
 		lcge_window_clear();
 
-		// Get ready for next iteration
+		lcge_image_draw(player_sprite);
+
+		/* Get ready for next iteration */
 		lcge_clock_tick(clock);
 		lcge_window_update();
 	}
 
-	// Clean up
+	/* Clean up */
 	lcge_clock_delete(clock);
+
+	delete_sprites();
 
 	lcge_exit();
 
